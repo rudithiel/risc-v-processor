3bcb997a8d70650605a6d49ca64bf6b3f6a1eef3
Rudi Thiel <rudi@rudithiel.com>
2023-05-26 14:50:05 +0200

    Update forwarding_unit.v

--- CA_Exercises/Verilog/RTL/forwarding_unit.v
+++ CA_Exercises/Verilog/RTL/forwarding_unit.v
@@ -10,29 +10,39 @@ module forwarding_unit(
     );
 
     always @(*) begin
-        // Default: No forwarding.
-        rs1_forward = 2'b00; 
-        rs2_forward = 2'b00;
-
-        // Forwarding from EX/MEM stage.
-        if ((reg_write_EX_MEM == 1'b1 && rd_EX_MEM == rs1 && rd_EX_MEM != 5'b0))
+        if (rs1 == rd_EX_MEM && reg_write_EX_MEM)
             rs1_forward = 2'b10;
+        else if (rs1 == rd_MEM_WB && reg_write_MEM_WB)
+            rs1_forward = 2'b01;
         else
             rs1_forward = 2'b00;
 
-        if (reg_write_EX_MEM == 1'b1 && rd_EX_MEM == rs2 && rd_EX_MEM != 5'b0)
+        if (rs2 == rd_EX_MEM && reg_write_EX_MEM)
             rs2_forward = 2'b10;
-        else 
+        else if (rs2 == rd_MEM_WB && reg_write_MEM_WB)
+            rs2_forward = 2'b01;
+        else
             rs2_forward = 2'b00;
 
-        // Forwarding from MEM/WB stage.
-        if ((reg_write_MEM_WB && (rd_MEM_WB != 5'b0) && (rd_MEM_WB == rs1)) && 
-            !(reg_write_EX_MEM && (rd_EX_MEM != 5'b0) && (rd_EX_MEM == rs1))
-            )
-            rs1_forward = 2'b01;
-        if ((reg_write_MEM_WB && (rd_MEM_WB != 5'b0000) && (rd_MEM_WB == rs2)) && 
-            !(reg_write_EX_MEM && (rd_EX_MEM != 5'b0000) && (rd_EX_MEM == rs2)))
-            rs2_forward = 2'b01;
+        // // Default: No forwarding.
+        // rs1_forward = 2'b00; 
+        // rs2_forward = 2'b00;
+
+        // // Forwarding from EX/MEM stage.
+        // if ((reg_write_EX_MEM == 1'b1 && rd_EX_MEM == rs1 && rd_EX_MEM != 5'b0))
+        //     rs1_forward = 2'b10;
+
+        // if (reg_write_EX_MEM == 1'b1 && rd_EX_MEM == rs2 && rd_EX_MEM != 5'b0)
+        //     rs2_forward = 2'b10;
+
+        // // Forwarding from MEM/WB stage.
+        // if ((reg_write_MEM_WB && (rd_MEM_WB != 5'b0) && (rd_MEM_WB == rs1)) && 
+        //     !(reg_write_EX_MEM && (rd_EX_MEM != 5'b0) && (rd_EX_MEM == rs1))
+        //     )
+        //     rs1_forward = 2'b01;
+        // if ((reg_write_MEM_WB && (rd_MEM_WB != 5'b0000) && (rd_MEM_WB == rs2)) && 
+        //     !(reg_write_EX_MEM && (rd_EX_MEM != 5'b0000) && (rd_EX_MEM == rs2)))
+        //     rs2_forward = 2'b01;
         
     end
 endmodule
